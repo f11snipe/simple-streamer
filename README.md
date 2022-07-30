dbc0b342fbe8d9f9c4ed245ce13a165b38f2f7c2
F11snipe <f11snipe@gmail.com>
2022-07-30 01:32:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# simple-streamer
+
+*Credit to: https://github.com/coding-with-chaim/webrtc-one-to-many*
+
+### Quickstart
+```bash
+# Install packages
+npm install
+
+# Run server
+npm start
+```
+
+For viewer screen: http://localhost:5000/
+
+For streamer screen: http://localhost:5000/stream.html
---
+++ public/stream.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+    document.getElementById('start').onclick = () => {
+        init();
+        document.getElementById('start').style.display = 'none';
+        document.getElementById('video').style.display = 'block';
+    }
+}
+
+async function init() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: {
+            width: 1920,
+            height: 1080,
+        }
+    });
+    document.getElementById("video").srcObject = stream;
+    const peer = createPeer();
+    stream.getTracks().forEach(track => peer.addTrack(track, stream));
+}
+
+
+function createPeer() {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
+
+    return peer;
+}
+
+async function handleNegotiationNeededEvent(peer) {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    const payload = {
+        sdp: peer.localDescription
+    };
+
+    const { data } = await axios.post('/broadcast', payload);
+    const desc = new RTCSessionDescription(data.sdp);
+    peer.setRemoteDescription(desc).catch(e => console.log(e));
+}
+
+
